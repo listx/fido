8d3138b5db08f246dec7a2cec58b13b5a29041b8
Linus Arver <linusarver@gmail.com>
2014-10-31 15:26:30 -0700

    login: successful login: redirect to user's own page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,8 @@ class SessionsController < ApplicationController
     # User model's 'has_secure_password' gives us the '#authenticate' method.
     if !user.nil? && user.authenticate(params[:session][:password])
       # Log in and redirect to the user's dashboard page.
+      log_in user
+      redirect_to user # same as `redirect_to user_path(user)`
     else
       # Create an error message.
       flash.now[:danger] = "Invalid username/password combination."
