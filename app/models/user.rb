b5790bef83e964ccddd8b50bc1a75dd84134909e
Linus Arver <linusarver@gmail.com>
2014-10-31 11:05:25 -0700

    user: add basic validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
-  validates :name, presence: true, uniqueness: true
+  validates :name,
+    presence: true,
+    length: {minimum: 2, maximum: 20},
+    uniqueness: {case_sensitive: false}
   has_secure_password
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "user attributes must not be empty" do
+    # Create an empty user object (everything set to 'nil').
+    user = User.new
+    # Such an empty user should not be able to pass validation.
+    assert user.invalid?
+    # An empty user object must have errors for the various attributes that
+    # cannot be empty.
+    assert user.errors[:name].any?
+    assert user.errors[:password].any?
+  end
+  test "should not create user if name is not unique case-insensitively" do
+    # The user "BOB" should fail because it is not unique case-insensitively
+    # against user "bob" in test/fixtures/users.yml.
+    user = User.new name: "BOB", password_digest: BCrypt::Password.create('ok')
+
+    assert user.invalid?
+  end
 end
