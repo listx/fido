16a8f13e62717846a90c17805fd5c499db8c358f
Linus Arver <linusarver@gmail.com>
2014-10-31 22:57:21 -0700

    'rake db:migrate'

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141101041924) do
+ActiveRecord::Schema.define(version: 20141101055152) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: true do |t|
+    t.integer  "author"
+    t.string   "title"
+    t.text     "body"
+    t.boolean  "published"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "roles", force: true do |t|
     t.string   "description"
