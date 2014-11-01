fffda276c0d35dc50e0b7537d2bb2f9564e714e2
Linus Arver <linusarver@gmail.com>
2014-10-31 20:57:08 -0700

    role: add seed data
    This is actually very useful for deployment because it defines standard
    roles that will always be there.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Populate standard roles
+Role.delete_all
+
+Role.create! description: "site_admin"
+Role.create! description: "author"
