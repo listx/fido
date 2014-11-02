5fd3b05fc58adceb6fdc3bd4c453043ebe9b434a
Linus Arver <linusarver@gmail.com>
2014-11-02 06:45:54 -0800

    posts: only logged in users can perform CRUD operations

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
 
-  before_action :logged_in_user, only: [:edit, :update, :destroy]
+  before_action :logged_in_user, only: [:new, :edit, :update, :destroy]
 
   before_action :correct_user, only: [:edit, :update, :destroy]
 
