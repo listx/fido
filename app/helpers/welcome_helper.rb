51496ab964e573fd5f7883ce6620c564a7df102a
Linus Arver <linusarver@gmail.com>
2014-11-04 22:41:08 -0800

    welcome#index: prettify post blurbs

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,2 +1,10 @@
 module WelcomeHelper
+  def truncate_paragraph(str, link_path)
+    pars = str.split("\n\n")
+    link = ""
+    if pars.size > 1
+      link = link_to("(read_more)", link_path)
+    end
+    [pars[0], link]
+  end
 end
