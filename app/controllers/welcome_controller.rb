489b327d88b9663b51d050069b31a4649b88302b
Linus Arver <linusarver@gmail.com>
2014-11-04 22:20:01 -0800

    welcome#index: add paginated posts and list of tenants in sidebar

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,6 @@
 class WelcomeController < ApplicationController
   def index
+    @tenants = Tenant.all
+    @posts = Post.where(published: true).paginate(page: params[:page], per_page: 10)
   end
 end
