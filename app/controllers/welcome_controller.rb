4c4205e985aa18f9d87881f2f0c06c59c6a0d65b
Linus Arver <linusarver@gmail.com>
2014-11-07 00:30:37 -0800

    welcome controller: sort tenant names (for sidebar)

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-    @tenants = Tenant.all
+    @tenants = Tenant.all.sort { |a,b| a.name <=> b.name }
     @posts = Post.where(published: true).paginate(page: params[:page], per_page: 10)
   end
 end
