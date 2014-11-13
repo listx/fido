b112b24c2e54a113d2239dffef24c4dcc9e65b4e
Linus Arver <linusarver@gmail.com>
2014-11-13 06:40:33 -0800

    perf.sh: rewrite in ruby

---
+++ perf.rb
@@ -0,0 +1,46 @@
+#!/usr/bin/env ruby
+# Run apache bench (ab) and generate a graph.
+
+require 'shell'
+
+url = ARGV[0]
+sh = Shell.new
+version = sh.system("rake db:version").to_s.split(" ").last
+t = Time.now.strftime("%Y%m%d-%H%M%S%Z")
+
+if url.nil?
+  puts "need url"
+  exit
+end
+
+outfile_data = "out.perf.#{version}.#{url.to_s.gsub(/\//, "!")}.#{t}.data.txt"
+outfile_meta = "out.perf.#{version}.#{url.to_s.gsub(/\//, "!")}.#{t}.meta.txt"
+outfile_pict = "out.perf.#{version}.#{url.to_s.gsub(/\//, "!")}.#{t}.pict.png"
+
+# Command arguments to ab
+# -k Enable the HTTP KeepAlive feature, i.e., perform multiple requests within
+#    one HTTP session.
+# -c concurrency: Number of multiple requests to perform at a time.
+# -n requests: Number of requests to perform for the benchmarking session.
+# -g gnuplot-compatible output
+sh.system("ab -k -c 10 -n 100 -g #{outfile_data} #{url}") > outfile_meta
+puts "Finished running ab"
+
+puts "Generating gnuplot graph..."
+
+format = "png"
+
+plot_cmd = %{\
+set term #{format} small
+set title "Benchmark of page\\n\
+#{url}"
+set xlabel "request"
+set ylabel "ms"
+set size 1,0.5
+set key left top
+plot "#{outfile_data}" using 10 with lines title "#{url}"
+}
+
+sh.echo(plot_cmd) | sh.system("gnuplot") > outfile_pict
+
+puts $?
